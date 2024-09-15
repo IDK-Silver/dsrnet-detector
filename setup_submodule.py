21eb88014f579c8946aec45bf5ca1c2ba379defb
AIGO <a288235403@gmail.com>
2024-09-16 00:10:35 +0800

    fix git not clone

--- setup_submodule.py
+++ setup_submodule.py
@@ -1,6 +1,6 @@
 import subprocess
 
 subprocess.run(["git", "submodule", "init"])
-
+subprocess.run(["git", "submodule", "update", '--recursive'])
 with open('src/dsrnet_detector/extern/DSRNet/__init__.py', 'w') as export:
     export.write("")
